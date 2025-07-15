387e73bb6deea2a5f930050a749da99fe73e6916
Eshwary Panthangi <panthangi@achala.local>
2025-07-15 10:02:18 +0200

    userdata updated

--- main.tf
+++ main.tf
@@ -49,15 +49,14 @@ resource "aws_instance" "web" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install nginx1 -y
-              systemctl start nginx
-              systemctl enable nginx
-              echo "<h1>Hello from NGINX + Terraform + GitHub Actions</h1>" > /usr/share/nginx/html/index.html
-              EOF
-
+user_data = <<-EOF
+  #!/bin/bash
+  apt update -y
+  apt install nginx -y
+  systemctl start nginx
+  systemctl enable nginx
+  echo "<h1>Hello from NGINX + Terraform + GitHub Actions </h1>" > /var/www/html/index.html
+EOF
   tags = {
     Name = "NginxWebServer"
   }
