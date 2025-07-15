fa234f9e892efc260f56fedaee002cc1a5ce65fc
Eshwary Panthangi <panthangi@achala.local>
2025-07-15 09:54:02 +0200

    port 22 enabled

--- main.tf
+++ main.tf
@@ -28,6 +28,14 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
   egress {
     from_port   = 0
     to_port     = 0
